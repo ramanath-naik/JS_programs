863ae0f8a1c08cdad91840fba7b4f819574673d9
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-19 18:26:31 +0530

    commit

---
+++ function.js
@@ -0,0 +1,19 @@
+//function
+
+function myfun(x,y)
+{
+    return x*y;
+}
+
+//another way of declaring a function
+const sum=(p,q)=>{
+    return (p+q);
+
+}
+
+console.log("multiplication of 2 and 4 is ",myfun(2,4))
+console.log("multiplication of 7 and 8 is ",myfun(7,8))
+console.log("multiplication of 9 and 8 is ",myfun(9,8))
+
+console.log("sum: ",sum(5,85))
+
