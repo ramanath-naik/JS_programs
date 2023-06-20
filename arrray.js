66bac46891c6aaa578cb2689f95adffe96e5ec23
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-20 18:00:53 +0530

    arrcommit

---
+++ arrray.js
@@ -0,0 +1,14 @@
+//array in javascript
+
+let arr1=[23,45,67,12,17]
+arr1[5]=77  //adding elements to array
+console.log(arr1)
+
+let arr2=[14,"mango","apple",45,89]
+console.log(arr2)
+
+//convert array to string
+let a1=[23,34,66,88]
+let s1=a1.toString()
+console.log(s1)
+console.log(typeof s1)
