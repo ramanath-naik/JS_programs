fccb5def76346b550f618e5e30a75c80f5e274ff
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 12:57:41 +0530

    commit

--- default.js
+++ default.js
@@ -6,3 +6,16 @@ function multiply(a, b = 1) {
   console.log(multiply(5, 2));//parameter passing
   
   console.log(multiply(5)); //only one paarameter is passing to the function
+
+  //passing function value as default value
+
+  // using a function in default value expression
+
+const sum = () => 15;
+
+const calculate = ( x, y = x * sum() ) =>  {
+    return x + y;
+}
+
+const result = calculate(10);
+console.log(result);            
