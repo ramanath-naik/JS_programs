170707f625084ab3c06071323a0a1a2cafdcf709
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 11:44:43 +0530

    secondcommit

--- arrowarguments.js
+++ arrowarguments.js
@@ -11,3 +11,10 @@ x(16,17,18)  //arguments
 //     console.log(arguments);
 // }
 // y(6,7,8)
+
+// To solve this issue we can use spread syntax
+let y = (...n) => {
+    console.log(n);
+}
+
+x(4,6,7);
