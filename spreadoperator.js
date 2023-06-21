b1d092c23f91b2d4b7493fa40347f49af12d3b19
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 15:23:04 +0530

    comm

---
+++ spreadoperator.js
@@ -0,0 +1,11 @@
+//Spread operator
+
+function sum(x, y, z) {
+    return x + y + z;
+  }
+  
+  const numbers = [1, 2, 3];
+  
+  console.log(sum(...numbers));
+  
+  console.log(sum.apply(null, numbers));
