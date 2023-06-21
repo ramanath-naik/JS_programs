8aa34ad1844a12b3b20aced3078db74380d2318b
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 13:29:03 +0530

    commit

--- defaultex.js
+++ defaultex.js
@@ -9,3 +9,18 @@ function fun(x,y) {
    fun( 0, 33 ); 
    console.log("with undefined tupe")
    fun( undefined, 7 ); 
+
+   //example 2
+
+   function bar(val) {
+    console.log( "bar called!" );
+    return y + val;
+   }
+   function foo(x = y + 3, z = bar( x )) {
+    console.log( x, z );
+   }
+   var y = 5;
+   foo(); 
+   foo( 10 );
+   y = 6;
+   foo( undefined, 10 ); 
