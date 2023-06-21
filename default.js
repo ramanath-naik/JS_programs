40ae144a28d1553a35231c3b4add6b165933e2d6
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 12:55:48 +0530

    commit

---
+++ default.js
@@ -0,0 +1,8 @@
+//Default parameters
+
+function multiply(a, b = 1) {
+    return a * b;
+  }
+  console.log(multiply(5, 2));//parameter passing
+  
+  console.log(multiply(5)); //only one paarameter is passing to the function
