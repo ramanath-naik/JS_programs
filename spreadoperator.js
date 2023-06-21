0cd60dad70a06d643efb13ec820bb3217d7ed31a
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 15:48:39 +0530

    commit

--- spreadoperator.js
+++ spreadoperator.js
@@ -3,9 +3,13 @@
 function sum(x, y, z) {
     return x + y + z;
   }
-  
   const numbers = [1, 2, 3];
   
   console.log(sum(...numbers));
   
   console.log(sum.apply(null, numbers));
+//converting array to object using spread operator
+let arr1=[1,2,3]
+let obj={...arr1}
+console.log(obj)
+
