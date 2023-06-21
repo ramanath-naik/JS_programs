b9af829321890de52b3499130b3f01a3060c5750
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 16:13:56 +0530

    commit

--- spreadoperator.js
+++ spreadoperator.js
@@ -13,3 +13,16 @@ let arr1=[1,2,3]
 let obj={...arr1}
 console.log(obj)
 
+//array literals (spread operator)
+const parts = ["shoulders", "knees"];
+const lyrics = ["head", ...parts, "and", "toes"];
+console.log(lyrics)
+
+//spread in object literals
+const ob1={nm:"anand",ag:23}
+const ob2={nm:"aditya",a:24}
+
+const cloneob={...ob1}
+const mergeob={...ob1, ...ob2}
+console.log(cloneob)
+console.log(mergeob)
