29f23c45199c5181eefed8bff5aaf6fdc71c4bdc
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-21 12:08:36 +0530

    comm

--- templiterals.js
+++ templiterals.js
@@ -5,3 +5,17 @@ const myfun = (name,age) => {
 
 }
 myfun("aditya",23);
+
+// using the + operator
+const message1 = 'This is a long message\n' + 
+'that spans across multiple lines\n' + 
+'in the code.'
+
+console.log(message1)
+
+//using literals
+const message2 = `This is a long message
+that spans across multiple lines
+in the code.`
+
+console.log(message1)
