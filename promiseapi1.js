596385c703095c83896e41a516c2550da48b9d46
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-22 16:33:53 +0530

    commit

--- promiseapi1.js
+++ promiseapi1.js
@@ -2,7 +2,8 @@
 
 let p1 = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        resolve("value 1")
+        // resolve("value 1")
+        reject(new Error("error")); //It will show error
     }, 1000)
 })
 
@@ -18,9 +19,15 @@ let p3 = new Promise((resolve,reject)=>{
     }, 3000)
 })
 
-p1.then((value)=>{console.log(value)})
+// p1.then((value)=>{console.log(value)})
 
-p2.then((value)=>{console.log(value)})
+// p2.then((value)=>{console.log(value)})
 
-p3.then((value)=>{console.log(value)})
+// p3.then((value)=>{console.log(value)})
 
+//better we can use promise.all api
+
+let promise_all = Promise.all([p1,p2,p3])
+promise_all.then((value)=>{
+    console.log(value)
+})
