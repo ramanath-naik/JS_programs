4eb4c439f4b2bcd0b514041e9f3e46dfcf67f1bc
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-23 12:46:16 +0530

    commit

--- iife.js
+++ iife.js
@@ -8,3 +8,23 @@
 })();
 
 //first variable and second variable will be discarded after the execution of the function.
+
+// normal function
+let f1=()=>{
+    age=30;
+    console.log("his age is :",age)
+
+}
+f1();
+console.log(age);
+
+
+//IIFE example
+
+((value)=>{
+    let a=22;
+    console.log(value);
+    console.log(a)
+})("hello");
+// console.log(value);
+// console.log(a);
