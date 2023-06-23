f592592dc3457a884e74986e4f9edcc77aa3146d
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-23 18:38:53 +0530

    commit

--- applycallbind.js
+++ applycallbind.js
@@ -1,6 +1,24 @@
 // apply, call, bind in javascript
 
 //bind()
+// var employee = {
+//     firstname: 'Ramu',
+//     lastname: 'Naik ',
+//     getempName: function() {
+//         var fullname = this.firstname + ' ' + this.lastname;
+//         return fullname;
+//     }
+// }
+
+// var employeeName = function() {
+//     console.log(this.getempName() + 'I know you!');
+// }
+
+// var showemp= employeeName.bind(employee); // creates new object and binds employee. 
+
+// showemp(); 
+
+//bind with arguments
 var employee = {
     firstname: 'Ramu',
     lastname: 'Naik ',
@@ -10,12 +28,10 @@ var employee = {
     }
 }
 
-var employeeName = function() {
-    console.log(this.getempName() + 'I know you!');
+var employeeName = function(friend) {
+    console.log(this.getempName() + 'I know your friend is '+friend);
 }
 
-var showemp= employeeName.bind(employee); // creates new object and binds employee. 
+var showemp= employeeName.bind(employee); 
 
-showemp(); 
-
-//
+showemp("Pramath"); //we can pass multiple arguments
