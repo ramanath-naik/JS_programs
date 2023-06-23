26a7bb14ccfba561c6f1281d5eb0791dc2e07127
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-23 12:16:46 +0530

    commit

---
+++ iife.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expression in javascript
+
+//declaration
+
+(()=>{
+    let firstvariable;
+    let secondvariable;
+})();
+
+//first variable and second variable will be discarded after the execution of the function.
