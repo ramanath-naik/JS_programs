26148df5e0af79a0899fd2df21f9774b0eb66e3d
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-23 16:20:53 +0530

    commit

--- hoist.js
+++ hoist.js
@@ -1,8 +1,28 @@
 // Hoisting in javascript
 
 
+//following line will run successfully due to javascript hoisting.
+console.log(a)  //but it is undefined
 greet() //hoisting
 function greet()
 {
     console.log("Good morning")
 }
+
+var a=30;  // declaration hoisted to the top but not initialization
+console.log(a)
+
+
+//let and const with hoisting
+// It will show error 
+// console.log(b)
+// console.log(d)
+
+// function bd()
+// {
+//     console.log("hello")
+// }
+
+// let b=22
+// const d=11
+
