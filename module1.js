4703c9e47a55c7a3dead69510636aeef401a3a1b
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-26 14:38:44 +0530

    commit

---
+++ module1.js
@@ -0,0 +1,19 @@
+//module pattern in javascript
+//exporting module
+
+export const hello = ()=>
+{
+    console.log("Hello Ramanath");
+
+}
+
+export const hello1 = (Name)=>
+{
+    console.log("Hi " + Name);
+}
+
+const greet = ()=>
+{
+    console.log("good morning");
+}
+export default greet;
