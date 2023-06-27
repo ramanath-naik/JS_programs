cb4aa619205ef5d6f7a22f5765a02bedc02d7280
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-27 10:28:18 +0530

    commit

---
+++ polymorphism2.js
@@ -0,0 +1,15 @@
+//example for polymorphism
+// unlike other languages, javascript does not allow overloading.
+
+// class math{
+//     sum(a,b){
+//         return (a+b);
+//     }
+//     sum(a,b,c){
+//         return a+b+c;
+//     }
+// }
+
+// const  obj = new math();
+// console.log(obj.sum(12,20));
+// console.log(obj.sum(10,20,30));
