689f8331aca676a81cb4b1bb38aa16c7da626185
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2023-06-27 16:03:24 +0530

    commit

--- callbackex.js
+++ callbackex.js
@@ -33,6 +33,68 @@
 
 //this will print newdata 
 
+// const datas=[
+//     {Name:"aditya",profession:"Software Developer"},
+//     {Name:"anand",profession:"Software Developer"}    
+// ];
+
+// function getdatas()
+// {
+//     setTimeout(()=>{
+//         let output="";
+//         // let output=[];
+//         datas.forEach((data,index)=>{      //we can also use datas.map
+//             // output.push(data.Name);
+//             output+=`${data.Name}\n`
+//         })
+//         console.log(output);
+//     },3000)  // timeout is greater than newdata function.
+// }
+
+// function createdata(newdata){
+//     setTimeout(()=>{
+//         datas.push(newdata);
+
+//     },2000)
+// }
+// createdata({Name:"akshay",profession:"Software Developer"})
+// getdatas();
+
+
+//how we can use callback for above program
+
+// const datas=[
+//     {Name:"aditya",profession:"Software Developer"},
+//     {Name:"anand",profession:"Software Developer"}    
+// ];
+
+// function getdatas()
+// {
+//     setTimeout(()=>{
+//         let output="";
+//         // let output=[];
+//         datas.forEach((data,index)=>{      //we can also use datas.map
+//             // output.push(data.Name);
+//             output+=`${data.Name}\n`
+//         })
+//         console.log(output);
+//     },1000)
+// }
+
+// function createdata(newdata,callback){
+//     setTimeout(()=>{
+//         datas.push(newdata);
+//         callback();
+
+//     },2000)
+// }
+// createdata({Name:"akshay",profession:"Software Developer"},getdatas)
+
+//all the values printed without updating the timeout value.
+
+
+//using promise, we can resolve problem.
+
 const datas=[
     {Name:"aditya",profession:"Software Developer"},
     {Name:"anand",profession:"Software Developer"}    
@@ -48,7 +110,7 @@ function getdatas()
             output+=`${data.Name}\n`
         })
         console.log(output);
-    },3000)  // timeout is greater than newdata function.
+    },1000)
 }
 
 function createdata(newdata){
